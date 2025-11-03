76be10b50e2f07a7b18101d5a1ac649598bcf8e0
nima64 <kennethe407@gmail.com>
2025-11-02 17:27:41 -0800

    test

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -18,7 +18,8 @@ success_tests! {
     // fun_iseven: {file:"fun_iseven", input: "99", expected: "99
 // false
 // false"}
-    fun_iseven: {file:"fun_iseven", input: "99", expected: "99\nfalse\nfalse"},
+    // fun_iseven: {file:"fun_iseven", input: "99", expected: "99\nfalse\nfalse"},
+    fun_iseven: {file:"fun_iseven", input: "99", expected: "false"},
     fun_print: {file:"print", input: "", expected: "3
 3"},
     fun_print_input: {file:"print_input", input: "99", expected: "99
@@ -26,20 +27,20 @@ success_tests! {
 }
 
 runtime_error_tests! {
-    integer_overflow_add1: { file: "integer_overflow_add1", input: "4611686018427387903", expected: "integer overflow" },
-    integer_overflow_sub1: { file: "integer_overflow_sub1", input: "-4611686018427387904", expected: "integer overflow" },
-    integer_overflow_mul: { file: "integer_overflow_mul", input: "2", expected: "integer overflow" },
-    integer_overflow_mul_negative: { file: "integer_overflow_mul_negative", input: "2", expected: "integer overflow" },
-    integer_overflow_plus: { file: "integer_overflow_plus", input: "", expected: "integer overflow" },
-    integer_overflow_minus: { file: "integer_overflow_minus", input: "", expected: "integer overflow" },
+    // integer_overflow_add1: { file: "integer_overflow_add1", input: "4611686018427387903", expected: "integer overflow" },
+    // integer_overflow_sub1: { file: "integer_overflow_sub1", input: "-4611686018427387904", expected: "integer overflow" },
+    // integer_overflow_mul: { file: "integer_overflow_mul", input: "2", expected: "integer overflow" },
+    // integer_overflow_mul_negative: { file: "integer_overflow_mul_negative", input: "2", expected: "integer overflow" },
+    // integer_overflow_plus: { file: "integer_overflow_plus", input: "", expected: "integer overflow" },
+    // integer_overflow_minus: { file: "integer_overflow_minus", input: "", expected: "integer overflow" },
 }
 
 static_error_tests! {
     // test_parse_error: { file: "parse", input: "2", expected: "Invalid" },
-    test_loop_break_error: { file: "loop_break", input: "", expected: "break outside of loop" },
+    // test_loop_break_error: { file: "loop_break", input: "", expected: "break outside of loop" },
 }
 
 
 repl_tests! {
-    test_simple_bools: ["(define x true)", "x", "false"] => ["true", "false"],
+    // test_simple_bools: ["(define x true)", "x", "false"] => ["true", "false"],
 }
