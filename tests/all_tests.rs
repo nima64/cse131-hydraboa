f99de35d9a788eeb1f12988c506244a9c6b2cefb
nima64 <kennethe407@gmail.com>
2025-11-02 17:10:55 -0800

    func tests

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -3,22 +3,26 @@ mod infra;
 // Your tests go here!
 success_tests! {
     // test_input: { file: "input", input: "2", expected: "2" },
-    nested_loop: { file: "nested_loop", input: "", expected: "-6" },
-    binop_plus: { file: "plus", input: "", expected: "8" },
-    binop_minus: { file: "minus", input: "", expected: "6" },
-    binop_times: { file: "times", input: "", expected: "42" },
-    binop_less: { file: "less", input: "", expected: "true" },
-    binop_greater: { file: "greater", input: "", expected: "true" },
-    binop_less_equal: { file: "less_equal", input: "", expected: "true" },
-    binop_greater_equal: { file: "greater_equal", input: "", expected: "true" },
-    binop_equal: { file: "equal", input: "", expected: "true" },
-    unop_add1: { file: "add1", input: "", expected: "4" },
-    unop_sub1: { file: "sub1", input: "", expected: "-4" },
+    // nested_loop: { file: "nested_loop", input: "", expected: "-6" },
+    // binop_plus: { file: "plus", input: "", expected: "8" },
+    // binop_minus: { file: "minus", input: "", expected: "6" },
+    // binop_times: { file: "times", input: "", expected: "42" },
+    // binop_less: { file: "less", input: "", expected: "true" },
+    // binop_greater: { file: "greater", input: "", expected: "true" },
+    // binop_less_equal: { file: "less_equal", input: "", expected: "true" },
+    // binop_greater_equal: { file: "greater_equal", input: "", expected: "true" },
+    // binop_equal: { file: "equal", input: "", expected: "true" },
+    // unop_add1: { file: "add1", input: "", expected: "4" },
+    // unop_sub1: { file: "sub1", input: "", expected: "-4" },
     fun_fact: {file:"fact", input: "9", expected: "362880"},
-    fun_iseven: {file:"fun_iseven", input: "99", expected: "99
-false
-false"}
-
+//     fun_iseven: {file:"fun_iseven", input: "99", expected: "99
+// false
+// false"}
+    fun_iseven: {file:"fun_iseven", input: "", expected: "false"},
+    fun_print: {file:"print", input: "", expected: "3
+3"},
+    fun_print_input: {file:"print_input", input: "99", expected: "99
+99"}
 }
 
 runtime_error_tests! {
