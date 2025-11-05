ea9eeca3383f9b63da1049d01a059ccbd26cd02b
nima64 <kennethe407@gmail.com>
2025-11-05 13:51:30 -0800

    mutiple args test

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -2,24 +2,20 @@ mod infra;
 
 // Your tests go here!
 success_tests! {
-    // test_input: { file: "input", input: "2", expected: "2" },
-    // nested_loop: { file: "nested_loop", input: "", expected: "-6" },
-    // binop_plus: { file: "plus", input: "", expected: "8" },
-    // binop_minus: { file: "minus", input: "", expected: "6" },
-    // binop_times: { file: "times", input: "", expected: "42" },
-    // binop_less: { file: "less", input: "", expected: "true" },
-    // binop_greater: { file: "greater", input: "", expected: "true" },
-    // binop_less_equal: { file: "less_equal", input: "", expected: "true" },
-    // binop_greater_equal: { file: "greater_equal", input: "", expected: "true" },
-    // binop_equal: { file: "equal", input: "", expected: "true" },
-    // unop_add1: { file: "add1", input: "", expected: "4" },
-    // unop_sub1: { file: "sub1", input: "", expected: "-4" },
+    test_input: { file: "input", input: "2", expected: "2" },
+    nested_loop: { file: "nested_loop", input: "", expected: "-6" },
+    binop_plus: { file: "plus", input: "", expected: "8" },
+    binop_minus: { file: "minus", input: "", expected: "6" },
+    binop_times: { file: "times", input: "", expected: "42" },
+    binop_less: { file: "less", input: "", expected: "true" },
+    binop_greater: { file: "greater", input: "", expected: "true" },
+    binop_less_equal: { file: "less_equal", input: "", expected: "true" },
+    binop_greater_equal: { file: "greater_equal", input: "", expected: "true" },
+    binop_equal: { file: "equal", input: "", expected: "true" },
+    unop_add1: { file: "add1", input: "", expected: "4" },
+    unop_sub1: { file: "sub1", input: "", expected: "-4" },
     fun_fact: {file:"fact", input: "9", expected: "362880"},
-    // fun_iseven: {file:"fun_iseven", input: "99", expected: "99
-// false
-// false"}
-    // fun_iseven: {file:"fun_iseven", input: "99", expected: "99\nfalse\nfalse"},
-    fun_iseven: {file:"fun_iseven", input: "99", expected: "false"},
+    fun_iseven: {file:"fun_iseven", input: "99", expected: "99\nfalse\nfalse"},
     fun_print_iseven: {file:"fun_print_iseven", input: "99", expected: "false\nfalse"},
     fun_print: {file:"print", input: "", expected: "3
 3"},
@@ -27,21 +23,21 @@ success_tests! {
 99"},
     fun_chain: {file:"fun_chain", input: "", expected: "19
 19"},
-    fun_mutual_recursion: {file:"fun_mutual_recursion", input: "", expected: "25\n25"}
+    fun_mutual_recursion: {file:"fun_mutual_recursion", input: "", expected: "25\n25"},
+    fun_mutiple_args: {file:"fun_mutiple_args", input: "", expected: "3628800"}
 }
 
 runtime_error_tests! {
-    // integer_overflow_add1: { file: "integer_overflow_add1", input: "4611686018427387903", expected: "integer overflow" },
-    // integer_overflow_sub1: { file: "integer_overflow_sub1", input: "-4611686018427387904", expected: "integer overflow" },
-    // integer_overflow_mul: { file: "integer_overflow_mul", input: "2", expected: "integer overflow" },
-    // integer_overflow_mul_negative: { file: "integer_overflow_mul_negative", input: "2", expected: "integer overflow" },
-    // integer_overflow_plus: { file: "integer_overflow_plus", input: "", expected: "integer overflow" },
-    // integer_overflow_minus: { file: "integer_overflow_minus", input: "", expected: "integer overflow" },
+    integer_overflow_add1: { file: "integer_overflow_add1", input: "4611686018427387903", expected: "integer overflow" },
+    integer_overflow_sub1: { file: "integer_overflow_sub1", input: "-4611686018427387904", expected: "integer overflow" },
+    integer_overflow_mul: { file: "integer_overflow_mul", input: "2", expected: "integer overflow" },
+    integer_overflow_mul_negative: { file: "integer_overflow_mul_negative", input: "2", expected: "integer overflow" },
+    integer_overflow_plus: { file: "integer_overflow_plus", input: "", expected: "integer overflow" },
+    integer_overflow_minus: { file: "integer_overflow_minus", input: "", expected: "integer overflow" },
 }
 
 static_error_tests! {
-    // test_parse_error: { file: "parse", input: "2", expected: "Invalid" },
-    // test_loop_break_error: { file: "loop_break", input: "", expected: "break outside of loop" },
+    test_loop_break_error: { file: "loop_break", input: "", expected: "break outside of loop" },
     duplicate_function_name: { file: "duplicate_fun", input: "", expected: "Multiple functions are defined with the same name" },
     undefined_function: { file: "undefined_fun", input: "", expected: "" },
     input_in_function: { file: "input_in_fun", input: "5", expected: "input" },
@@ -50,5 +46,5 @@ static_error_tests! {
 
 
 repl_tests! {
-    // test_simple_bools: ["(define x true)", "x", "false"] => ["true", "false"],
+    test_simple_bools: ["(define x true)", "x", "false"] => ["true", "false"],
 }
